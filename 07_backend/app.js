d0e83ae749d867cc350003df0bb0b458a627f4e8
ArzoRaza <ccoding76@gmail.com>
2024-05-31 22:53:50 +0530

    some changes are here

--- 07_backend/app.js
+++ 07_backend/app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ exrended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get('/', (req, res) => {
-    res.send("hello arzo");
+    res.render("index");
+}) 
+app.get('/read', (req, res) => {
+    res.render("read");
 }) 
 
-app.listen(1);
+
+app.listen(3000);
