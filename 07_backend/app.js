2092d8495d75aceec5953f01f1ec30d3c9bbac35
ArzoRaza <ccoding76@gmail.com>
2024-06-01 10:19:47 +0530

    CRUD application completed

--- 07_backend/app.js
+++ 07_backend/app.js
@@ -17,6 +17,15 @@ app.get('/read', async (req, res) => {
     let users = await userModel.find();
     res.render("read", {users});
 }) 
+app.get('/edit/:userid', async (req, res) => {
+    let user = await userModel.findOne({_id: req.params.userid});
+    res.render("edit", {user});
+}) 
+app.post('/update/:userid', async (req, res) => {
+    let {image, name, email} = req.body;
+    let user = await userModel.findOneAndUpdate({_id: req.params.userid}, {email, name, image}, {new:true});
+    res.redirect("/read");
+}) 
 
 app.get('/delete/:id', async (req, res) => {
     let users = await userModel.findOneAndDelete({_id: req.params.id});
