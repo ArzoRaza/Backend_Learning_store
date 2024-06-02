4fb28d7344ae6ca6aa121ec4eb142789ffdd8de4
ArzoRaza <ccoding76@gmail.com>
2024-06-02 23:38:18 +0530

    bcrypt completed

--- 08_auth_backend/app.js
+++ 08_auth_backend/app.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const app = express();
 const bcrypt = require('bcrypt');
+const { JsonWebTokenError } = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 
 // app.use(cookieParser());
@@ -16,10 +19,23 @@ const bcrypt = require('bcrypt');
 //     res.send("reload done");
 // })
 
-app.get("/", function(req, res) {
-    bcrypt.compare("password", "$2b$10$uyokqhmt73S7LJKOyPmn4un9KIPA/Iyhx7oH5uPBtLc6IPSwI8p1.", function(err, result){
-        console.log(result);
-    })
+// app.get("/", function(req, res) {
+//     bcrypt.compare("password", "$2b$10$uyokqhmt73S7LJKOyPmn4un9KIPA/Iyhx7oH5uPBtLc6IPSwI8p1.", function(err, result){
+//         console.log(result);
+//     })
+// })
+
+app.use(cookieParser());
+
+app.get("/", function (req, res) {
+    let token = jwt.sign({email: "akba@gmail.com"}, "secret");
+    res.cookie("token", token);
+    res.send("ho gya bhiya")
+})
+
+app.get("/read", function(req, res) {
+    let data = jwt.verify(req.cookies.token, "secret");
+    console.log(data);
 })
 
 app.listen(3000);
