5dc50a0d767810dc1cfe3d0553e959c09519e4bf
ArzoRaza <ccoding76@gmail.com>
2024-06-03 01:14:39 +0530

    some changes

--- 09_auth_backend/app.js
+++ 09_auth_backend/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
-const path = require('path');
+const userModel = require("./models/user");
+
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
 
 app.set("view engine", "ejs");
@@ -11,7 +13,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
-    res.send("welcome arzo");
+    res.render("index");
 });
+
+app.get("/read", (req, res) => {
+    res.send("akbar try and try again")
+});
+
+app.post("/create", async (req, res) => {
+    let {username, email, password, age} = req.body;
+    let createdUser = await userModel.create({
+        username,
+        email,
+        password,
+        age
+    });
+    res.send(createdUser);
+});
+
+
 
 app.listen(3000);
