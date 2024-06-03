c38542227911357f1d9f5f42f9d71633ff20fdc9
ArzoRaza <ccoding76@gmail.com>
2024-06-03 19:30:58 +0530

    file just accomplish

--- 09_auth_backend/app.js
+++ 09_auth_backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const userModel = require("./models/user");
+const bcrypt = require('bcrypt');
 
 const cookieParser = require('cookie-parser');
 const path = require('path');
+const jwt = require('jsonwebtoken');
 
 
 app.set("view engine", "ejs");
@@ -16,20 +18,56 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
+
 app.get("/read", (req, res) => {
     res.send("akbar try and try again")
 });
 
-app.post("/create", async (req, res) => {
+app.post('/create',  (req, res) => {
     let {username, email, password, age} = req.body;
-    let createdUser = await userModel.create({
-        username,
-        email,
-        password,
-        age
-    });
-    res.send(createdUser);
-});
+
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, async(err, hash) => {
+            let createdUser = await userModel.create({
+                username,
+                email,
+                password: hash,
+                age
+            });
+
+            let token = jwt.sign({email}, "loooloo");
+            res.cookie("token", token);
+            res.send(createdUser);
+        });
+            
+    })
+
+})
+
+app.get("/login", function(req, res ){
+    res.render('login')
+})
+
+app.post("/login", async function(req, res ){
+    let user = await userModel.findOne({email:req.body.email});
+    if(!user) return res.send("somethig went wrong?")
+    
+    bcrypt.compare(req.body.password, user.password , function (err, result){
+        if(result) {
+            let token = jwt.sign({email: user.email}, "loooloo");
+            res.cookie("token", token);
+            res.send("yes you can login");
+        }
+        else res.send("somethig went wrong")
+    })
+})
+
+
+
+app.get("/logout", function(req, res){
+    res.cookie("token", "");
+    res.redirect('/');
+})
 
 
 
--- 09_auth_backend/models/user.js
+++ 09_auth_backend/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb://127.0.0.1:27017//teasapp`);
+mongoose.connect(`mongodb://localhost`);
+
 
 const userSchema = mongoose.Schema({
     username: String,
