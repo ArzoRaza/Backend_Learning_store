a88a1009515fdcfa58d85f790652b021d1389cb3
ArzoRaza <ccoding76@gmail.com>
2024-06-05 19:45:59 +0530

    these folders and file have have store  MINI project

---
+++ 11_miniProject/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Arzo you need hardwork every days not single day you put your 12 hours and thorgh you will won it's never happen ever ");
+});
+
+app.listen(3000);
+
