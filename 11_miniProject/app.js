1e3ae400dc6867c7815a573cd1fe715ba60ac2b1
ArzoRaza <ccoding76@gmail.com>
2024-06-05 20:01:33 +0530

    some changes happend

--- 11_miniProject/app.js
+++ 11_miniProject/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
+const userModel = require('./models/user');
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended: ture}));
+
 
 app.get('/', (req, res) => {
-    res.send("Arzo you need hardwork every days not single day you put your 12 hours and thorgh you will won it's never happen ever ");
+    res.send("hello Arzo");
+});
+
+app.post('/create', (req, res) => {
+    res.send("hello MD Akbar");
 });
 
 app.listen(3000);
